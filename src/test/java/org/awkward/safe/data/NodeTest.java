5925ebeee565ddc4abede04c9d60d07ba7f4dcfe
tbeauvais <thomas.beauvais@hotmail.com>
2014-07-27 19:08:59 +0200

    master: And..

---
+++ src/test/java/org/awkward/safe/data/NodeTest.java
@@ -0,0 +1,63 @@
+package org.awkward.safe.data;
+
+import com.sun.jna.platform.win32.VerRsrc;
+import org.awkward.DefaultUnitTest;
+import org.awkward.safe.model.DirectoryNode;
+import org.awkward.safe.model.FileNode;
+import org.awkward.safe.model.Version;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * TODO:  Please document properly all classes and methods using the Silbury JavaDoc Guidelines
+ * TODO:    provided on the Silbury Confluence under Silbury JavaDoc Standards Guidelines.
+ *
+ * @author Silbury Solutions, Deutschland - Thomas Beauvais (thomas.beauvais@silbury.de)
+ * @since 27.07.14
+ */
+@ContextConfiguration(locations = "classpath:spring.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class NodeTest extends DefaultUnitTest
+{
+    @Autowired
+    FileNodeRepository nodeRepository;
+
+    @Autowired
+    DirectoryNodeRepository directoryNodeRepository;
+
+    @Autowired
+    VersionRepository versionRepository;
+
+    @Test
+    public void path()
+    {
+        final Version version = versionRepository.findByPathOrderByLastModifiedAsc("/safe/Code/personal/safe/data/local");
+
+        System.out.println(version);
+    }
+
+    @Test
+    public void test()
+    {
+        assertNotNull(nodeRepository);
+        assertNotNull(directoryNodeRepository);
+
+        final DirectoryNode root = local.getRoot();
+
+        directoryNodeRepository.save(root);
+    }
+
+    @Test
+    public void retrieve()
+    {
+        final List<DirectoryNode> all = directoryNodeRepository.findAll();
+        System.out.println(all);
+    }
+}
